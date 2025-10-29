cd87398ac7ff91f59f74f75c15faba0be941d343
Farsos Bulsara <16597052+kingsukhoi@users.noreply.github.com>
2025-10-28 22:37:35 -0600

    fix kong

--- cmd/qbtPanel/main.go
+++ cmd/qbtPanel/main.go
@@ -7,11 +7,12 @@ import (
 
 func main() {
 	ctx := kong.Parse(&commands.CLI,
+		kong.Bind(&commands.CLI.Globals),
 		kong.Name("qbittorrent-panel"),
 		kong.Description("A CLI tool to manage qBittorrent instances"),
 		kong.UsageOnError(),
 	)
 
-	err := ctx.Run(&commands.CLI)
+	err := ctx.Run()
 	ctx.FatalIfErrorf(err)
 }
