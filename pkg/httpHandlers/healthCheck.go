040decdaae5b3b402bedc2e55c4fc477438a7e0e
Farsos Bulsara <16597052+kingsukhoi@users.noreply.github.com>
2026-04-14 05:06:41 -0600

    report unhealthy if unable to get version

--- pkg/httpHandlers/healthCheck.go
+++ pkg/httpHandlers/healthCheck.go
@@ -3,10 +3,20 @@ package httpHandlers
 import (
 	"net/http"
 
+	"github.com/kingsukhoi/qbitorrent-panel/pkg/qbClient"
 	"github.com/labstack/echo/v4"
 )
 
 func HealthCheck(c echo.Context) error {
+	clients := qbClient.Registry().All()
+
+	for _, client := range clients {
+		_, err := client.GetVersion(c.Request().Context())
+		if err != nil {
+			return err
+		}
+	}
+
 	return c.JSON(http.StatusOK, map[string]string{
 		"status": "ok",
 	})
