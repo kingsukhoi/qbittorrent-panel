b5e762b1b7f7d25354385a15017d6954d81ccf9d
Farsos Bulsara <16597052+kingsukhoi@users.noreply.github.com>
2026-04-18 11:50:19 -0600

    fix main go logger

--- cmd/graphqlServer/main.go
+++ cmd/graphqlServer/main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"context"
 	"errors"
 	"log/slog"
 	"net/http"
 	"os"
-	"os/signal"
-	"time"
 
 	"github.com/kingsukhoi/qbitorrent-panel/pkg/configuration"
 	"github.com/kingsukhoi/qbitorrent-panel/pkg/qbClient"
@@ -39,53 +36,10 @@ func main() {
 	// Create Echo instance
 	e := routers.NewEchoHandler(h)
 
-	if cfg.GetEnv() == configuration.EnvDev {
-		e.Debug = true
-	}
-
-	// Root context is cancelled on Ctrl+C / SIGINT
-	rootCtx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
-	defer stop()
-
-	// Start server
-	go func() {
-		err := e.Start(cfg.Port)
-		if err != nil && !errors.Is(err, http.ErrServerClosed) {
-			e.Logger.Fatal("shutting down the server")
+	err := e.Start(cfg.Port)
+	if err != nil {
+		if !errors.Is(err, http.ErrServerClosed) {
+			panic(err)
 		}
-	}()
-
-	// start qbittorrent client keep alive (stops on shutdown)
-	go func(ctx context.Context) {
-		ticker := time.NewTicker(30 * time.Minute)
-		defer ticker.Stop()
-
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			case <-ticker.C:
-				clients := qbClient.Registry().All()
-				for _, client := range clients {
-					reqCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
-					_, err := client.GetVersion(reqCtx)
-					cancel()
-
-					if err != nil {
-						slog.Error("qbittorrent keep-alive failed", "basePath", client.BasePath.String(), "err", err)
-					}
-				}
-			}
-		}
-	}(rootCtx)
-
-	// Wait for interrupt signal
-	<-rootCtx.Done()
-
-	// Graceful shutdown with timeout
-	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	if err := e.Shutdown(shutdownCtx); err != nil {
-		e.Logger.Fatal(err)
 	}
 }
